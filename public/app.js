e6bd9d1bfc0da605f708d47efd11adfb3816a9d3
Richard Clarke <freshnewrich@gmail.com>
2022-04-11 10:22:30 -0700

    styles

--- public/app.js
+++ public/app.js
@@ -1,1 +1,2 @@
 console.log("i exist")
+// alert("Dev Note\n The index tiles are actually really cool, they have a PNG of a Gameboy Color with a 0% opacity screen area, and you can see the pokemon image 'through' the screen. Also, I created five different color Gameboy Colors that are randomly assigned to the index tiles.")
